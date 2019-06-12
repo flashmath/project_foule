2970f709a66a7e7af1d8ab2e74ad5c5d77019574
naldegheri <naldegheri@yahoo.fr>
2019-06-12 23:21:02 +0200

    Définition de la sous-classe Voyageur

--- src/voyageur.py
+++ src/voyageur.py
@@ -7,7 +7,7 @@ class Point():
         self.y = y       
 
     def __str__(self):
-        return '(' + str(self.x) + ";" + str(self.y) + ')'
+        return 'Coordonnées du point : ' + '(' + str(self.x) + ";" + str(self.y) + ')'
         
 class Vecteur():
     """
@@ -18,20 +18,36 @@ class Vecteur():
         self.vy = vy       
 
     def __str__(self):
-        return '(' + str(self.vx) + ";" + str(self.vy) + ')'
+        return 'Vecteur vitesse : ' + '(' + str(self.vx) + ";" + str(self.vy) + ')'
         
 class Element():
     """
     une classe pour définir les éléments sur la carte
     """
-    def __init__(self,positionX,positionY):     # Position courante de l'objet dans la carte
+    def __init__(self,positionX,positionY):     # Position de l'objet dans la carte
         self.posX = positionX         
         self.posY = positionY     
     
     def __str__(self):
-        return '(' + str(self.posX) + ";" + str(self.posY) + ')'
+        return 'Position : ' + '(' + str(self.posX) + ";" + str(self.posY) + ')'
 
 class Obstacle(Element):
     """
     une classe pour définir un Obstacle sur la carte
     """
+    
+class Voyageur(Element):
+    """
+    une classe pour définir un Voyageur sur la carte
+    """
+    def __init__(self,positionX,positionY,destinationX,destinationY):     # Position courante et destination
+        self.posX = positionX         
+        self.posY = positionY
+        self.destX = destinationX         
+        self.destY = destinationY     
+    
+    def __str__(self):
+        return 'Position courante : ' + '(' + str(self.posX) + ";" + str(self.posY) + ')' + '\n' + 'Destination : ' + '(' + str(self.destX) + ";" + str(self.destY) + ')'
+
+v=Voyageur(10,11,12,13)
+print(v)
