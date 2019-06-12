039924b8ca64b6fec37a5f07ed90401531835ba9
naldegheri <naldegheri@yahoo.fr>
2019-06-12 16:17:26 +0200

    Ajout de la classe Point

---
+++ src/voyageur.py
@@ -0,0 +1,7 @@
+class Point():
+    """
+    une classe pour définir les coordonnées d'un point
+    """
+    def __init__(self,x,y):
+        self.x = x         #self, c'est soit même 
+        self.y = y         #self, c'est soit même 
