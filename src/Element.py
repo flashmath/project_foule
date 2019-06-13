4bd3098fc9c18b0a470f37b194259789d0ac8569
fabrice <flash_math@yahoo.fr>
2019-06-13 10:04:26 +0200

    Ajout du nouvel élément

---
+++ src/Element.py
@@ -0,0 +1,19 @@
+class Element:
+    """
+            Classe gérant les éléments d'une carte
+        """
+
+    def __init__(self, depart, map):
+        """
+                    Initialise un voyageur
+
+                    :param x: position en x du voyageur
+                    :param y: position en y du voyageur
+                    :type x: float
+                    :type y: float
+                    :return: retourne le voyageur
+
+                    .. warning:: classe peu implémentée à finir
+                """
+        self.map = map
+        self.position = depart
---
+++ src/voyageur.py
@@ -0,0 +1,20 @@
+class Voyageur:
+    """
+        Classe gérant un voyageur.
+    """
+
+    def __init__(self, x, y):
+        """
+            Initialise un voyageur
+
+            :param x: position en x du voyageur
+            :param y: position en y du voyageur
+            :type x: float
+            :type y: float
+            :return: retourne le voyageur
+
+            .. warning:: classe peu implémentée à finir
+        """
+        # TODO: finir la définition de la classe voyageur
+        self.x = x
+        self.y = y
